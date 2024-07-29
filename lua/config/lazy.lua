a851535a807e583017513deacb37276a30406b73
Josu <56826352+josugoar@users.noreply.github.com>
2024-07-29 22:50:21 +0200

    Use new defaults

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,7 +24,10 @@ require("lazy").setup({
     version = false,
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
   performance = {
     rtp = {
       disabled_plugins = {
