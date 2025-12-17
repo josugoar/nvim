8a8ab0d645707507d55d12b9c30e61b008c1b54b
Josu <56826352+josugoar@users.noreply.github.com>
2025-12-17 17:13:54 +0100

    Always use linewise comments in vscode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,29 @@ vim.keymap.set("n", "<leader>gp", '"+gp', { desc = "Put Text From Clipboard Afte
 vim.keymap.set("n", "<leader>gP", '"+gP', { desc = "Put Text From Clipboard Before Selection" })
 vim.keymap.set("n", "<leader>]p", '"+]p', { desc = "Put Indented From Clipboard After Cursor" })
 vim.keymap.set("n", "<leader>[p", '"+[p', { desc = "Put Indented From Clipboard Before Cursor" })
+
+if not vim.g.vscode then
+  return
+end
+
+local vscode = require("vscode")
+
+local function esc()
+  local key = vim.api.nvim_replace_termcodes("<esc>", true, true, true)
+  vim.api.nvim_feedkeys(key, "n", false)
+end
+
+local k = function(mode, lhs, rhs)
+  vim.keymap.set(mode, lhs, rhs, { expr = true })
+end
+
+local comment = vscode.to_op(function(ctx)
+  local cmd = "editor.action.commentLine"
+  local opts = { range = nil, callback = esc }
+  if ctx.is_current_line then
+    opts.range = nil
+  end
+  vscode.action(cmd, opts)
+end)
+
+k({ "n", "x" }, "gc", comment)
